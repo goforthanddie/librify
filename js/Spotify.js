c0bd7ee001b524e8f2de506090a22291be467559
samy <elshamy@coderbutze.de>
2023-04-29 10:56:53 +0200

    moved some stuff to init.js and moved auth functions into Spotify.js

--- js/Spotify.js
+++ js/Spotify.js
@@ -1,14 +1,25 @@
+const clientId = 'f77bc91de5834f398680d65c02bdfe94';
+//const redirectUri = 'https://librify.coderbutze.de';
+const redirectUri = 'http://localhost:63342/SpotifyTree/index.html';
+
 class Spotify {
+
     artists;
 
     constructor() {
-        this.artists = new Array();
+        let artists = localStorage.getItem('artists');
+        if(artists != null) {
+            this.artists = JSON.parse(artists);
+            this.populateViewLibraryFromArtists(this.artists);
+        } else {
+            this.artists = new Array();
+        }
         this.accessToken = null;
         this.arrayGenres = new Array();
         this.arrayDevices = new Array();
     }
 
-    getAccessToken() { // aktualisieren mit neuer methode
+    getAccessTokenOld() { // aktualisieren mit neuer methode
         $.ajax({
             url: 'https://accounts.spotify.com/api/token',
             type: 'POST',
@@ -22,7 +33,7 @@ class Spotify {
                 'state': ''
                 //'client_secret': 'b99d199fb18d461b8828cf0f9205c455'
             },
-            success: function(data) {
+            success: function (data) {
                 console.log(this);
                 this.accessToken = new AccessToken(data.access_token, data.token_type, data.expires_in);
                 console.log(this);
@@ -33,7 +44,7 @@ class Spotify {
         });
     }
 
-    getDevices() {
+    getDevices() { // Todo: create object class
         $.ajax({
             url: 'https://api.spotify.com/v1/me/player/devices',
             type: 'GET',
@@ -41,7 +52,7 @@ class Spotify {
             headers: {
                 'Authorization': this.accessToken.type + '  ' + this.accessToken.token
             },
-            success: function(data) {
+            success: function (data) {
                 this.arrayDevices = data.devices;
                 Spotify.populateSelectDevices(this.arrayDevices);
                 console.log('getDevices()');
@@ -60,7 +71,7 @@ class Spotify {
     }
 
     getSavedAlbums(offset = 0, limit = 50) {
-        console.log('getSavedAlbums('+offset+','+limit+')');
+        console.log('getSavedAlbums(' + offset + ',' + limit + ')');
         //console.log(this);
         //console.log(this.accessToken);
         $.ajax({
@@ -74,7 +85,7 @@ class Spotify {
             headers: {
                 'Authorization': this.accessToken.type + '  ' + this.accessToken.token
             },
-            success: function(data) {
+            success: function (data) {
                 console.log(data);
                 // iterate over returned albums
                 data.items.forEach(item => {
@@ -89,6 +100,7 @@ class Spotify {
                             artst.addAlbum(album);
                             this.artists.push(artst);
                         } else { // artist id found, add album to existing artist
+                            console.log(this.artists[idArtist]);
                             this.artists[idArtist].addAlbum(album);
                         }
                     });
@@ -96,16 +108,16 @@ class Spotify {
                     // iterate over genres of album
                     item.album.genres.forEach(genre => {
                         this.arrayGenres.push(genre);
-                        if(!this.arrayGenres.includes(genre)) {
+                        if (!this.arrayGenres.includes(genre)) {
                             this.arrayGenres.push(genre);
                         }
                     });
                 });
 
                 // test if there are more albums
-                if(data.next != null) {
+                if (data.next != null) {
                     console.log('get more albums');
-                    this.getSavedAlbums(offset+limit, limit);
+                    this.getSavedAlbums(offset + limit, limit);
                 } else { // no more albums
                     console.log('no more albums');
                     this.artists.sort((a, b) => a.name.localeCompare(b.name));
@@ -125,19 +137,19 @@ class Spotify {
 
     startPlayback(albumId) {
         $.ajax({
-            url: 'https://api.spotify.com/v1/me/player/play?device_id='+$('#selectDevices').children(':selected').attr('id'),
+            url: 'https://api.spotify.com/v1/me/player/play?device_id=' + $('#selectDevices').children(':selected').attr('id'),
             type: 'PUT',
             context: this,
             data:
                 JSON.stringify({
-                'context_uri': 'spotify:album:'+albumId,
-                'position_ms': 0,
+                    'context_uri': 'spotify:album:' + albumId,
+                    'position_ms': 0,
 
-            }),
+                }),
             headers: {
                 'Authorization': this.accessToken.type + '  ' + this.accessToken.token
             },
-            success: function(data) {
+            success: function (data) {
                 console.log(data);
             }
         });
@@ -159,7 +171,7 @@ class Spotify {
             spanArtistName.classList.add('caret');
             spanArtistName.classList.add('expandable');
 
-            spanArtistName.addEventListener('click',() => {
+            spanArtistName.addEventListener('click', () => {
                 ulAlbums.classList.toggle('active');
                 spanArtistName.classList.toggle('expandable');
                 spanArtistName.classList.toggle('collapsable');
@@ -183,6 +195,95 @@ class Spotify {
         });
     }
 
+    /**
+     * Generates a random string containing numbers and letters
+     * @param  {number} length The length of the string
+     * @return {string} The generated string
+     */
+    static generateRandomString(length) {
+        let text = '';
+        let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+        for (let i = 0; i < length; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+    }
+
+    static async generateCodeChallenge(codeVerifier) {
+        function base64encode(string) {
+            return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+                .replace(/\+/g, '-')
+                .replace(/\//g, '_')
+                .replace(/=+$/, '');
+        }
+
+        const encoder = new TextEncoder();
+        const data = encoder.encode(codeVerifier);
+        const digest = await window.crypto.subtle.digest('SHA-256', data);
+
+        return base64encode(digest);
+    }
+
+    static authorize() {
+        let codeVerifier = Spotify.generateRandomString(128);
+
+        Spotify.generateCodeChallenge(codeVerifier).then(codeChallenge => {
+            let state = Spotify.generateRandomString(16);
+            let scope = 'user-library-read user-modify-playback-state user-read-playback-state';
+
+            localStorage.setItem('code_verifier', codeVerifier);
+            console.log('set code_verifier=' + codeVerifier);
+
+            let args = new URLSearchParams({
+                response_type: 'code',
+                client_id: clientId,
+                scope: scope,
+                redirect_uri: redirectUri,
+                state: state,
+                code_challenge_method: 'S256',
+                code_challenge: codeChallenge
+            });
+
+            window.location = 'https://accounts.spotify.com/authorize?' + args;
+        });
+    }
+
+    getAccessToken(code) {
+
+        let codeVerifier = localStorage.getItem('code_verifier');
+
+        let body = new URLSearchParams({
+            grant_type: 'authorization_code',
+            code: code,
+            redirect_uri: redirectUri,
+            client_id: clientId,
+            code_verifier: codeVerifier
+        });
+        const response = fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: body
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error('HTTP status ' + response.status);
+            }
+            return response.json();
+        }).then(data => {
+            console.log('data');
+            console.log(data);
+
+            localStorage.setItem('access_token', data.access_token);
+            loginOk(this, data.access_token);
+        }).catch(error => {
+            console.error('Error:', error);
+            $('#login').show();
+            $('#loggedin').hide();
+        });
+    }
+
     populateViewLibrary(arrayLibraryDescriptive) {
         const ulLibrary = document.getElementById('ulLibrary');
         ulLibrary.innerHTML = '';
@@ -199,7 +300,7 @@ class Spotify {
             spanArtistName.classList.add('caret');
             spanArtistName.classList.add('expandable');
 
-            spanArtistName.addEventListener('click',() => {
+            spanArtistName.addEventListener('click', () => {
                 ulAlbums.classList.toggle('active');
                 spanArtistName.classList.toggle('expandable');
                 spanArtistName.classList.toggle('collapsable');
@@ -209,20 +310,20 @@ class Spotify {
             ulLibrary.appendChild(liArtist);
 
             arrayAlbums.forEach((albumName, albumId) => {
-               let liAlbum = document.createElement('li');
-               liAlbum.innerHTML = albumName;
-               liAlbum.addEventListener('click', () => {
-                  this.startPlayback(albumId);
-               });
-               ulAlbums.appendChild(liAlbum);
-           })
+                let liAlbum = document.createElement('li');
+                liAlbum.innerHTML = albumName;
+                liAlbum.addEventListener('click', () => {
+                    this.startPlayback(albumId);
+                });
+                ulAlbums.appendChild(liAlbum);
+            })
             liArtist.appendChild(ulAlbums);
         });
     }
 
     static getArrayLibraryDescriptive(arrayLibrary, arrayArtistIdName, arrayAlbumIdName) {
         const sortStringKeys = ([a], [b]) => String(a).localeCompare(b);
-        const sortStringValues = ([,a], [,b]) => String(a).localeCompare(b);
+        const sortStringValues = ([, a], [, b]) => String(a).localeCompare(b);
 
         const arrayLibraryDescriptive = new Map();
         // sort arrayArtistIdName alphabetically
--- js/init.js
+++ js/init.js
@@ -1,6 +1,51 @@
+function loginOk(spotify, access_token) {
+    $('#login').hide();
+    $('#loggedin').show();
+
+    //let spotify = new Spotify();
+    spotify.accessToken = new AccessToken(access_token, 'Bearer');
+    spotify.getDevices();
+    spotify.getSavedAlbums(2700, 50);
+
+    console.log('login ok');
+}
+
 // Listeners
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
     const spotify = new Spotify();
-    //spotify.getAccessToken();
-    //spotify.getSavedAlbums();
+    //$('#loggedin').hide();
+    //$('#selectDevices').hide();
+
+    const urlParams = new URLSearchParams(window.location.search);
+    let code = urlParams.get('code');
+
+    let access_token = localStorage.getItem('access_token');
+
+    // test if there is a code, meaning we got redirected from spotify auth page and no access_token
+    if (code && access_token == null) {
+        spotify.getAccessToken(code);
+    } else if (access_token != null) {
+        console.log('access_token != null');
+        // Todo: test if access_token is still valid?
+        loginOk(spotify, access_token);
+    }
+
+    document.getElementById('buttonReloadLibrary').addEventListener('click', function () {
+        spotify.getSavedAlbums(2650, 50);
+    }, false);
+
+    document.getElementById('buttonReloadDevices').addEventListener('click', function () {
+        spotify.getDevices();
+    }, false);
+
+    document.getElementById('buttonLogout').addEventListener('click', function () {
+        localStorage.clear();
+        $('#login').show();
+        $('#loggedin').hide();
+    }, false);
+
+    document.getElementById('buttonLogin').addEventListener('click', function () {
+        console.log('login-button:click()');
+        Spotify.authorize();
+    }, false);
 });
